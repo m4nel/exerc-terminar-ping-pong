8befd4b098bcf551e0aba0832fc31cf8cdc574c7
Emanue Angelo <emanuelangelo1@hotmail.com>
2022-05-23 15:09:35 -0300

    primeiro commit

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function calcule() {
+    
+    let contador = 0
+    while (contador < 100) {
+contador++
+
+        if(contador % 2 == 0){
+    console.log("ping")
+}
+
+
+else  {
+    console.log("pong")
+}
+
+
+    }
+
+
+
+}
